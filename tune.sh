a967efedf8230291747404b336b443ee8d465eb7
Borja Castellano <borjacastellano1@gmail.com>
2025-10-24 14:08:06 +0000

    notes updated

--- tune.sh
+++ tune.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# If not root exit
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
 if [ ! -d ".venv" ]; then
     if ! python3 -m venv .venv; then
         echo "Error creating python virtual environment. Install python3-venv."
